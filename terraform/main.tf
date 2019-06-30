96fdfd68834b9ef30efdc3298c300d8f7b6f9168
Rob Charlwood <robcharlwood@gmail.com>
2019-06-30 11:01:32 +0100

    Added main terraform to tie everything together.

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+  version     = "~> 2.1"
+  credentials = "${file(".keys/terraform_key.json")}"
+  project     = "${var.PROJECT}"
+  region      = "${var.REGION}"
+  zone        = "${var.ZONE}"
+}
+
+module "s3" {
+  source = "./s3"
+  REGION = "${var.REGION}"
+}
+
+module "pubsub" {
+  source = "./pubsub"
+}
+
+module "cloud-functions" {
+  source                = "./cloud-functions"
+  functions_bucket_name = "${module.s3.gcp-av-functions-bucket-name}"
+  scan_bucket_name      = "${module.s3.gcp-av-scan-bucket-name}"
+  pubsub_topic_name     = "${module.pubsub.gcp-av-pubsub-topic-name}"
+}
+
+module "scheduler" {
+  source          = "./scheduler"
+  REGION          = "${var.REGION}"
+  pubsub_topic_id = "${module.pubsub.gcp-av-pubsub-topic-id}"
+}
